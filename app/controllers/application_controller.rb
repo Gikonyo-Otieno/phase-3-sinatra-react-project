95e41826519073a8b8f22baddf730d76dcda7643
Gikonyo-Otieno <gikonyootieno@gmail.com>
2023-03-03 04:08:55 +0300

    Creating models and config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,56 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    events = Event.all #gets all events in array format so you need to convert to json
+    p events
+    events.to_json
+  end
+
+  #get single event
+  get "/event/:pk" do
+    event= Event.find_by(id: params[:pk])
+    event.to_json
+  end
+
+  #post event
+  post "/event/" do
+    event= Event.create(
+      name: params[:name],
+      date: params[:date],
+      venue: params[:venue],
+      time: params[:time],
+      price1: params[:price1],
+      price2: params[:price2]
+    )
+    event.save
+    {
+      "Message": "Created Successfully",
+      "Status": "HTTP_200_OK"
+    }.to_json
+  end
+
+  # Patch Event
+  patch "event/update/:id" do 
+    event= Event.find_by(id: params[:id])
+    event.update(
+      name: params[:name],
+      date: params[:date],
+      venue: params[:venue],
+      time: params[:time],
+      price1: params[:price1],
+      price2: params[:price2] 
+    )
+    event.to_json
+  end
+
+  #Delete a Event
+  delete "/event/:id" do
+    event= Event.find_by(id: params[:id])
+    event.destroy
+    {
+      "Message":"Successfully deleted an Event",
+      "Status":"HTTP_400"
+    }
   end
 
 end
